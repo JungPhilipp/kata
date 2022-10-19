6da18fa27dc98b505f89f1e7d7e82f1e31550f7c
Philipp Jung <philipp.jung@mailbox.org>
2022-10-19 11:02:07 +0200

    Refactor todays Kata

--- src/chop.rs
+++ src/chop.rs
@@ -3,27 +3,27 @@ pub fn functional_chop(target: i32, elements: &[i32]) -> Option<usize> {
 }
 
 pub fn recursive_chop(target: i32, elements: &[i32]) -> Option<usize> {
-    if elements.is_empty() {
-        return None;
-    }
-
-    if elements.len() == 1 {
-        if elements[0] == target {
-            return Some(0);
-        } else {
-            return None;
+    match elements.len() {
+        0 => None,
+        1 => {
+            if elements[0] == target {
+                Some(0)
+            } else {
+                None
+            }
         }
-    }
+        _ => {
+            let pivot_index = elements.len() / 2;
+            let pivot_element = elements[pivot_index];
 
-    let pivot_index = elements.len() / 2;
-    let pivot_element = elements[pivot_index];
-
-    match target {
-        x if x < pivot_element => recursive_chop(target, &elements[0..pivot_index]),
-        x if x == pivot_element => Some(pivot_index),
-        x if x > pivot_element => {
-            recursive_chop(target, &elements[pivot_index..]).map(|i| pivot_index + i)
+            match target {
+                x if x < pivot_element => recursive_chop(target, &elements[0..pivot_index]),
+                x if x == pivot_element => Some(pivot_index),
+                x if x > pivot_element => {
+                    recursive_chop(target, &elements[pivot_index..]).map(|i| pivot_index + i)
+                }
+                _ => panic!("Unreachable"),
+            }
         }
-        _ => panic!("Unreachable"),
     }
 }
