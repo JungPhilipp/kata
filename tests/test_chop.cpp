43ab7fd12380a0a5cc16334be6a88824c10eaa5f
Philipp Jung <philipp.jung@mailbox.org>
2022-10-19 12:16:24 +0200

    Add chop c++ kata

--- tests/test_chop.cpp
+++ tests/test_chop.cpp
@@ -7,30 +7,32 @@
 #include <span>
 
 namespace kata::test {
-TEST_CASE("Chop") {
+namespace {
+template <class chop_t> void test_chop(chop_t chop_function) {
+  CHECK_EQ(std::nullopt, chop_function(3, {}));
+  CHECK_EQ(std::nullopt, chop_function(3, std::to_array({1})));
+  CHECK_EQ(std::optional(0), chop_function(1, std::to_array({1})));
 
-  CHECK_EQ(std::nullopt, chop(3, {}));
-  CHECK_EQ(std::nullopt, chop(3, std::to_array({1})));
-  CHECK_EQ(std::optional(0), chop(1, std::to_array({1})));
+  CHECK_EQ(std::optional(0), chop_function(1, std::to_array({1, 3, 5})));
+  CHECK_EQ(std::optional(1), chop_function(3, std::to_array({1, 3, 5})));
+  CHECK_EQ(std::optional(2), chop_function(5, std::to_array({1, 3, 5})));
 
-  CHECK_EQ(std::optional(0), chop(1, std::to_array({1, 3, 5})));
-  CHECK_EQ(std::optional(1), chop(3, std::to_array({1, 3, 5})));
-  CHECK_EQ(std::optional(2), chop(5, std::to_array({1, 3, 5})));
+  CHECK_EQ(std::nullopt, chop_function(0, std::to_array({1, 3, 5})));
+  CHECK_EQ(std::nullopt, chop_function(2, std::to_array({1, 3, 5})));
+  CHECK_EQ(std::nullopt, chop_function(4, std::to_array({1, 3, 5})));
+  CHECK_EQ(std::nullopt, chop_function(6, std::to_array({1, 3, 5})));
 
-  CHECK_EQ(std::nullopt, chop(0, std::to_array({1, 3, 5})));
-  CHECK_EQ(std::nullopt, chop(2, std::to_array({1, 3, 5})));
-  CHECK_EQ(std::nullopt, chop(4, std::to_array({1, 3, 5})));
-  CHECK_EQ(std::nullopt, chop(6, std::to_array({1, 3, 5})));
+  CHECK_EQ(std::optional(0), chop_function(1, std::to_array({1, 3, 5, 7})));
+  CHECK_EQ(std::optional(1), chop_function(3, std::to_array({1, 3, 5, 7})));
+  CHECK_EQ(std::optional(2), chop_function(5, std::to_array({1, 3, 5, 7})));
+  CHECK_EQ(std::optional(3), chop_function(7, std::to_array({1, 3, 5, 7})));
 
-  CHECK_EQ(std::optional(0), chop(1, std::to_array({1, 3, 5, 7})));
-  CHECK_EQ(std::optional(1), chop(3, std::to_array({1, 3, 5, 7})));
-  CHECK_EQ(std::optional(2), chop(5, std::to_array({1, 3, 5, 7})));
-  CHECK_EQ(std::optional(3), chop(7, std::to_array({1, 3, 5, 7})));
-
-  CHECK_EQ(std::nullopt, chop(0, std::to_array({1, 3, 5, 7})));
-  CHECK_EQ(std::nullopt, chop(2, std::to_array({1, 3, 5, 7})));
-  CHECK_EQ(std::nullopt, chop(4, std::to_array({1, 3, 5, 7})));
-  CHECK_EQ(std::nullopt, chop(6, std::to_array({1, 3, 5, 7})));
-  CHECK_EQ(std::nullopt, chop(8, std::to_array({1, 3, 5, 7})));
+  CHECK_EQ(std::nullopt, chop_function(0, std::to_array({1, 3, 5, 7})));
+  CHECK_EQ(std::nullopt, chop_function(2, std::to_array({1, 3, 5, 7})));
+  CHECK_EQ(std::nullopt, chop_function(4, std::to_array({1, 3, 5, 7})));
+  CHECK_EQ(std::nullopt, chop_function(6, std::to_array({1, 3, 5, 7})));
+  CHECK_EQ(std::nullopt, chop_function(8, std::to_array({1, 3, 5, 7})));
 }
+} // namespace
+TEST_CASE("Chop") { test_chop(&chop); }
 } // namespace kata::test
